e9b9a5cc8f262e0e087215f14f93f2f5607cad8e
rudra1551 <rudramehsana1551@gmail.com>
2025-09-10 21:04:13 +0530

    Create Factorial of a no.c

---
+++ Factorial of a no.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n, fact=1;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    for(int i=1;i<=n;i++) {
+        fact *= i;
+    }
+    printf("Factorial = %d", fact);
+    return 0;
+}
