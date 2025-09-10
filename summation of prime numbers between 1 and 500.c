c3ad6677e0228a3f13141e2e816ef2e16ef97508
rudra1551 <rudramehsana1551@gmail.com>
2025-09-10 21:23:35 +0530

    Create summation of prime numbers between 1 and 500.c

---
+++ summation of prime numbers between 1 and 500.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int i, j, flag, sum=0;
+    for(i=2;i<=500;i++) {
+        flag=1;
+        for(j=2;j<=i/2;j++) {
+            if(i%j==0) { flag=0; break; }
+        }
+        if(flag) sum += i;
+    }
+    printf("Sum of primes = %d", sum);
+    return 0;
+}
