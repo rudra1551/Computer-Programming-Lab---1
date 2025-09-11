40b656716a5bc9862cd20911d37c786738f8e5c0
rudra1551 <rudramehsana1551@gmail.com>
2025-09-11 12:13:50 +0530

    Update Net salary.c

--- Net salary.c
+++ Net salary.c
@@ -3,19 +3,11 @@
 int main() {
     float gross_salary, net_salary;
     float allowances, deductions;
-
-    // Input gross salary
     printf("Enter the gross salary: ");
     scanf("%f", &gross_salary);
-
-    // Calculate allowances and deductions
-    allowances = 0.10 * gross_salary;  // 10% of gross salary
-    deductions = 0.07 * gross_salary;  // 7% of gross salary
-
-    // Calculate net salary
+    allowances = 0.10 * gross_salary;
+    deductions = 0.07 * gross_salary;
     net_salary = gross_salary + allowances - deductions;
-
-    // Display result
     printf("Net salary = %.2f\n", net_salary);
 
     return 0;
