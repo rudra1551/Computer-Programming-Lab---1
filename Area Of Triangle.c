b4fc75950919689db20281264b957fcadcbfe45f
rudra1551 <rudramehsana1551@gmail.com>
2025-09-11 12:12:45 +0530

    Update Area Of Triangle.c

--- Area Of Triangle.c
+++ Area Of Triangle.c
@@ -2,17 +2,11 @@
 
 int main() {
     float base, height, area;
-
-    // Input base and height
     printf("Enter the base of the triangle: ");
     scanf("%f", &base);
     printf("Enter the height of the triangle: ");
     scanf("%f", &height);
-
-    // Calculate area using A = (L * H) / 2
     area = (base * height) / 2;
-
-    // Display result
     printf("Area of the triangle = %.2f\n", area);
 
     return 0;
