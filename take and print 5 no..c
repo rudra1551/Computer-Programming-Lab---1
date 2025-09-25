00646626ef8550bd13f30886b0d3ef9cd622ec06
rudra1551 <rudramehsana1551@gmail.com>
2025-09-25 14:33:39 +0530

    Add files via upload

---
+++ take and print 5 no..c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int a[5],i;
+printf("enter 5 values");
+for(i=0;i<5;i++){
+scanf("%d",&a[i]);}
+printf("the values are:");
+for(i=0;i<5;i++){
+printf("%d",a[i]);}
+return 0;
+}
