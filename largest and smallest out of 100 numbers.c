6f8eccec85cae65f1dd7987c9415f9d9dc451b7a
rudra1551 <rudramehsana1551@gmail.com>
2025-10-29 21:46:50 +0530

    Update largest and smallest out of 100 numbers.c

--- largest and smallest out of 100 numbers.c
+++ largest and smallest out of 100 numbers.c
@@ -2,9 +2,8 @@
 int main() {
     int num, largest, smallest, i;
     printf("Enter 100 numbers:\n");
-    scanf("%d", &num);
     largest = smallest = num;
-    for(i=2;i<=100;i++) {
+    for(i=1;i<=100;i++) {
         scanf("%d", &num);
         if(num > largest) largest = num;
         if(num < smallest) smallest = num;
